66484b9682da8f3c58023fce526c4f8f0ca6027f
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2022-02-15 16:13:44 +0100

    downgrade package node version

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ env | grep DB_
 ./scripts/build-submodule.sh
 
 # ----Installing Subql-Node----
-npm install -g @subql/node
+npm install -g @subql/node@0.27.3-5
 
 # ----Installing dependencies----
 npm install
